8face43962df3f3d8b3c2975256f31faded4b932
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-22 23:36:46 +0800

    input validation is working. Raises value error if input is not numbers or numbers with decimals.

--- program1_Number_Sorter.py
+++ program1_Number_Sorter.py
@@ -10,4 +10,31 @@ def get_input():
     return first_input, second_input, third_input, fourth_input
 
 
+def input_validation(first_try, second_try, third_try, fourth_try):
+    if (first_try != "") and (second_try != "") and (third_try != "") and (fourth_try != ""):
+        try:
+            first_int, second_int, third_int, fourth_int = float(
+                first_try), float(second_try), float(third_try), float(fourth_try)
+            if (first_int - int(first_int) == 0):
+                first_int = int(first_int)
+
+            if (second_int - int(second_int) == 0):
+                second_int = int(second_int)
+
+            if (third_int - int(third_int) == 0):
+                third_int = int(third_int)
+            
+            if (fourth_int - int(fourth_int) == 0):
+                fourth_int = int(fourth_int)
+
+            return first_int, second_int, third_int, fourth_int
+
+        except ValueError:
+            print("\nError. Please enter numbers.")
+            return False, False, False, False
+    else:
+        print("\nEmpty input is invalid. Please try again.")
+        return False, False, False
+
 first, second, third, fourth = get_input()
+first_valid, second_valid, third_valid, fourth_valid = input_validation(first, second, third, fourth)
