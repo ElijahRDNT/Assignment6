cf5b16b9c4556306164e33ac59abf5b62369faa4
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-23 00:08:53 +0800

    added guide comments

--- program1_Number_Sorter.py
+++ program1_Number_Sorter.py
@@ -2,7 +2,7 @@
 # Create a program that ask 4 numbers.
 # Print the 4 numbers from highest to lowest using only if-else statement.
 
-def get_input():
+def get_input():    # function to collect inputs
     first_input = input("Good day!\n\nEnter first number: ")
     second_input = input("Enter second number: ")
     third_input = input("Enter third number: ")
@@ -11,12 +11,12 @@ def get_input():
 
 
 def input_validation(first_try, second_try, third_try, fourth_try):
-    if (first_try != "") and (second_try != "") and (third_try != "") and (fourth_try != ""):
+    if (first_try != "") and (second_try != "") and (third_try != "") and (fourth_try != ""):   # if inputs are not empty, the program continues
         try:
-            first_int, second_int, third_int, fourth_int = float(
+            first_int, second_int, third_int, fourth_int = float(       # converts inputs into float datatype to enable inputs with decimal points
                 first_try), float(second_try), float(third_try), float(fourth_try)
-            if (first_int - int(first_int) == 0):
-                first_int = int(first_int)
+            if (first_int - int(first_int) == 0):   # if the input is a whole number, it will be converted into int datatype to flash the original input back if it is the lowest
+                first_int = int(first_int)           # for example, without these codes, an input of 1 will be flashed as 1.0. But with this, input 1 will be flashed as 1
 
             if (second_int - int(second_int) == 0):
                 second_int = int(second_int)
@@ -30,15 +30,15 @@ def input_validation(first_try, second_try, third_try, fourth_try):
             return first_int, second_int, third_int, fourth_int
 
         except ValueError:
-            print("\nError. Please enter numbers.")
+            print("\nError. Please enter numbers.") # inputs that can't be converted to float datatype is restricted
             return False, False, False, False
     else:
-        print("\nEmpty input is invalid. Please try again.")
+        print("\nEmpty input is invalid. Please try again.")    # restricts empty inputs
         return False, False, False
+    # This time, negative inputs are allowed
 
-
-def number_order(first_no, second_no, third_no, fourth_no):
-    if first_no < second_no:
+def number_order(first_no, second_no, third_no, fourth_no): # this function is to classify the numbers from lowest to greatest value
+    if first_no < second_no:        # this time, I tried to tdo the classifying by pairs of numbers or inputs
         small_first_pair = first_no
         high_first_pair = second_no
     else:
@@ -72,10 +72,10 @@ def number_order(first_no, second_no, third_no, fourth_no):
         return (least_num, mid_num_b, mid_num_a, greatest_num)
 
 
-first, second, third, fourth = get_input()
+first, second, third, fourth = get_input()  # storing of inputs in different global variables
 first_valid, second_valid, third_valid, fourth_valid = input_validation(
     first, second, third, fourth)
-if (first_valid != False) and (second_valid != False) and (third_valid != False):
+if (first_valid != False) and (second_valid != False) and (third_valid != False) and (fourth_valid != False):   # if the input_validation() function did not return a false value, the program proceeds
     least, lesser, greater, greatest = number_order(
         first_valid, second_valid, third_valid, fourth_valid)
     print(
