b4e5e03d953a90f1a7aace7ef3391b72416dabca
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-23 00:41:34 +0800

    added a function for the score. it can also be directly printed in the print function in the last line but i decided to put it in a score computation function for a good practice.

--- program2_Addition_Trainer.py
+++ program2_Addition_Trainer.py
@@ -12,21 +12,36 @@ def system_header():
 
 
 def generate_number():
-    first_number = random.randint(0,99)
-    second_number = random.randint(0,99)
+    first_number = random.randint(0, 99)
+    second_number = random.randint(0, 99)
     return first_number, second_number
 
 
 def quiz_proper():
+    score = 0
     for question_number in range(1, 11):
         num_first, num_second = generate_number()
-        answer = float(input(f"{question_number}.)  {num_first} + {num_second} = "))
+        answer = float(
+            input(f"{question_number}.)  {num_first} + {num_second} = "))
         correct_answer = num_first + num_second
         if answer == correct_answer:
             print("Correct!\n")
-        
+            score = score + 1
+
         else:
             print(f"Incorrect. The correct answer is {correct_answer}.\n")
 
+    return score
+
+
+def score_computation(score_compute):
+    item_numbers = 10
+    final_score = str(f"{score_compute}\{item_numbers}")
+
+    return final_score
+
+
 system_header()
-quiz_proper()
+current_score = quiz_proper()
+final_rating = score_computation(current_score)
+print(f"Total score:    {final_rating}")
